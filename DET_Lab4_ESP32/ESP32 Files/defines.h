3fe29bddd6eafa3dfcf042ec80163de7e596eaea
Steelswarm <antoniomiguelbraga@live.com.pt>
2024-01-30 14:53:30 +0100

    Added Project files

---
+++ DET_Lab4_ESP32/ESP32 Files/defines.h
@@ -0,0 +1,52 @@
+/****************************************************************************************************************************
+  defines.h for ESP32-Client.ino
+  For ESP32.
+
+  Based on and modified from Gil Maimon's ArduinoWebsockets library https://github.com/gilmaimon/ArduinoWebsockets
+  to support STM32F/L/H/G/WB/MP1, nRF52 and SAMD21/SAMD51 boards besides ESP8266 and ESP32
+
+
+  The library provides simple and easy interface for websockets (Client and Server).
+
+  Built by Khoi Hoang https://github.com/khoih-prog/Websockets2_Generic
+  Licensed under MIT license
+ *****************************************************************************************************************************/
+
+#ifndef defines_h
+#define defines_h
+
+#if !( defined(ESP8266) ||  defined(ESP32) )
+#error This code is intended to run on the ESP32 platform! Please check your Tools->Board setting.
+#elif ( ARDUINO_ESP32S2_DEV || ARDUINO_FEATHERS2 || ARDUINO_ESP32S2_THING_PLUS || ARDUINO_MICROS2 || \
+        ARDUINO_METRO_ESP32S2 || ARDUINO_MAGTAG29_ESP32S2 || ARDUINO_FUNHOUSE_ESP32S2 || \
+        ARDUINO_ADAFRUIT_FEATHER_ESP32S2_NOPSRAM )
+#define BOARD_TYPE      "ESP32-S2"
+#elif ( ARDUINO_ESP32C3_DEV )
+#warning Using ESP32-C3 boards
+#define BOARD_TYPE      "ESP32-C3"
+#else
+#define BOARD_TYPE      "ESP32"
+#endif
+
+#ifndef BOARD_NAME
+  #define BOARD_NAME    BOARD_TYPE
+#endif
+
+#define DEBUG_WEBSOCKETS_PORT     Serial
+// Debug Level from 0 to 4
+#define _WEBSOCKETS_LOGLEVEL_     3
+
+//const char* ssid = "dsv-extrality-lab"; //Enter SSID
+//const char* password = "expiring-unstuck-slider"; //Enter Password
+
+const char* ssid = "dsv-extrality-lab"; //Enter SSID
+const char* password = "expiring-unstuck-slider"; //Enter Password
+
+//const char* websockets_server_host = "172.20.10.14"; //Enter server address
+const char* websockets_server_host = "10.204.0.6"; //Enter server address
+
+#define WEBSOCKETS_PORT     7890
+
+const uint16_t websockets_server_port = WEBSOCKETS_PORT; // Enter server port
+
+#endif      //defines_h
---
+++ DET_Lab4_Unity/Assets/ESP32 Lab/Scripts/WebSocketClient.cs
@@ -0,0 +1,86 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using NativeWebSocket;
+using System.Runtime.CompilerServices;
+
+public class WebSocketClient : MonoBehaviour
+{
+    [SerializeField]
+	private string IPAdress; //The IP Adress of the Server you want to connect to
+    [SerializeField]
+	private int Port = 32323; //The Port your WebSocket Connection will "talk" to
+    
+	private WebSocket webSocket;
+
+	private void initWebSocket() //Starts WebSocket Client Connection
+	{
+		webSocket = new WebSocket($"ws://{IPAdress}:{Port}");
+		webSocket.Connect();
+
+		webSocket.OnOpen += WebSocket_OnOpen;
+		webSocket.OnError += WebSocket_OnError;
+		webSocket.OnClose += WebSocket_OnClose;
+		webSocket.OnMessage += WebSocket_OnMessage;
+
+	}
+
+	private void WebSocket_OnOpen() //Alerts on console when WebSocket Connection is Successfull
+	{
+		Debug.Log("Connecion opened!");
+	}
+
+	private void WebSocket_OnError(string error) //Alerts on console when WebSocket Connection is Unsuccessfull
+	{
+		Debug.Log($"Error: {error}");
+	}
+
+	private void WebSocket_OnClose(WebSocketCloseCode closeCode) //Alerts on console when WebSocket Connection is Closed
+	{
+		Debug.Log("Connection closed!");
+	}
+
+	private void WebSocket_OnMessage(byte[] data) //Receives webSocket message and handles it respectively depending on what it contains
+	{
+		string socketMessage = System.Text.Encoding.UTF8.GetString(data);
+		//Debug.Log(System.Text.Encoding.UTF8.GetString(data));
+        Debug.Log(socketMessage);
+        
+	}
+	
+	
+		
+	async void SendWebSocketMessage(string text) //Sends Websocket Message to Server for the ESP32 to receive
+	{
+		if (webSocket.State == WebSocketState.Open)
+		{
+			// Sending plain text socket
+			await webSocket.SendText(text);
+		}
+	}
+	
+
+	// Start is called before the first frame update
+	void Start()
+	{
+		initWebSocket();
+		Debug.Log("Started");
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		webSocket.DispatchMessageQueue();
+
+    }
+	
+
+	private async void OnApplicationQuit() //Closes Websocket Connection Correctly when app is closed
+	{
+		await webSocket.Close();
+	}
+	
+}
+
+
